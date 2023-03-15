62481e40c0e097a4e73912a8f15c5d9e2a76358b
m305__2\m305 <joyce1223344@gmail.com>
2023-03-15 11:38:32 +0800

    增加與商人對話的UI互動(還沒做：商店選項、商店相關的資料儲存與數值設定)

--- Assets/Scripts/Move/01/PlayerControl.cs
+++ Assets/Scripts/Move/01/PlayerControl.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 public class PlayerControl : MonoBehaviour
 {
     public GameObject player;
+    public GameObject storeUI;
     CharacterController controller;
     public float speed = 10;
     float time = 0;
     public float randomMax, randomMin;
     public string Place;
     public string Aide;
+    bool StoreBool = false;
+    int e = 0;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -47,6 +50,28 @@ public class PlayerControl : MonoBehaviour
         // 移動 (x,z)
         Vector3 move = dir * speed * Time.deltaTime;
         controller.Move(move);
+
+        // 如果按了E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // 如果在商店範圍
+            if (StoreBool == true)
+            {
+                e += 1;
+                int _e = e % 2;
+                switch (_e)
+                {
+                    // 關商店
+                    case 0:
+                        storeUI.SetActive(false);
+                        break;
+                    // 開商店
+                    case 1:
+                        storeUI.SetActive(true);
+                        break;
+                }
+            }
+        }
     }
 
     //private void OnCollisionEnter(Collision collision)
@@ -57,6 +82,7 @@ public class PlayerControl : MonoBehaviour
     //        SceneManager.LoadScene(Place);
     //    }
     //}
+
     private void OnTriggerEnter(Collider other)
     {
         // 如果撞到地方就更改關卡
@@ -65,10 +91,61 @@ public class PlayerControl : MonoBehaviour
             SceneManager.LoadScene(Place);
         }
 
+        // 如果進入商人的對話範圍
+        if (other.gameObject.tag == "Store")
+        {
+            StoreBool= true;
+        }
+
         //    // 如果撞到敵人就更改關卡
         //    if (other.gameObject.tag == "Aide")
         //    {
         //        SceneManager.LoadScene(Aide);
         //    }
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Store")
+    //    {
+    //        //if (Input.GetKeyDown(KeyCode.E))
+    //        //{
+    //        //    e += 1;
+    //        //    switch (e)
+    //        //    {
+    //        //        case 0:
+    //        //            storeUI.SetActive(false);
+    //        //            break;
+    //        //        case 1:
+    //        //            storeUI.SetActive(true);
+    //        //            e = 0;
+    //        //            break;
+    //        //    }
+    //        //}
+
+    //        if (Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            e += 1;
+    //            int _e = e % 2;
+    //            switch (_e)
+    //            {
+    //                case 0:
+    //                    storeUI.SetActive(false);
+    //                    break;
+    //                case 1:
+    //                    storeUI.SetActive(true);
+    //                    break;
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 如果離開商人的對話範圍
+        if (other.gameObject.tag == "Store")
+        {
+            StoreBool = false;
+        }
     }
 }
